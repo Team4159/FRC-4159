25147a2ec955a040abf9ef4940f552542da273e1
U-Team4159-HP\Team4159 <Team4159@Team4159-HP.(none)>
2013-02-05 11:55:34 -0800

    add tick to test controller

--- src/org/team4159/frc2013/controllers/TestController.java
+++ src/org/team4159/frc2013/controllers/TestController.java
@@ -1,5 +1,6 @@
 package org.team4159.frc2013.controllers;
 
+import org.team4159.frc2013.Periodic;
 import org.team4159.support.Controller;
 import org.team4159.support.ModeEnumerator;
 
@@ -8,5 +9,10 @@ public class TestController extends Controller
 	public TestController ()
 	{
 		super (ModeEnumerator.TEST);
+	}
+	
+	public void tick ()
+	{
+		Periodic.tick ();
 	}
 }
